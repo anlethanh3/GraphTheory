d2c7a2c7a7f37373c722207e8d807c0ad4204f24
An Le <an.lethah3@gmail.com>
2016-03-18 00:53:30 +0700

    - all done version 0.1

--- Week1/src/AdjacencyMatrix.cpp
+++ Week1/src/AdjacencyMatrix.cpp
@@ -14,15 +14,66 @@ AdjacencyMatrix::~AdjacencyMatrix()
 
 int AdjacencyMatrix::CountEdges()
 {
-	return 0;
+	int sum = 0;
+	for(int i = 0; i < this->n; i++)
+	{
+		for(int j = i+1; j < this->n; j++)
+		{
+			sum += this->matrix[i][j];
+		}
+	}
+	return sum;
 }
 
 void 	AdjacencyMatrix::GetDegrees(int*& degrees)
 {
+	int degree;
+	degrees = new int[this->n];
+	for(int i = 0; i < this->n; i++)
+	{
+		degree = 0;
+		for(int j = 0; j < this->n; j++)
+		{
+			degree += this->matrix[i][j];
+		}
+		degrees[i] = degree;
+	}
 }
 
 int AdjacencyMatrix::GetTypeOfGraph()
 {
+	int result[3] 	= {0,0,0};
+	int* degrees; 
+	this->GetDegrees(degrees);
+	for(int i = 0; i < this->n; i++)
+	{
+		if(degrees[i] == this->n - 1)
+		{
+			result[0]++;
+		}
+		else if(degrees[i] == 2)
+		{
+			result[1]++;
+		}
+		else if(degrees[i] == 1)
+		{
+			result[2]++;
+		}
+	}
+	delete []degrees;
+	degrees = NULL;
+	if(result[0] == this->n)
+	{
+		return 1;
+	}
+	else if(result[1] == this->n)
+	{
+		return 2;
+	}
+	else if(result[0] == 1 && result[2] == this->n-1)
+	{
+		return 3;
+	}
 	return 0;
 }
 
@@ -45,5 +96,18 @@ void AdjacencyMatrix::ReadInput(char* file)
 
 void AdjacencyMatrix::PrintOutput(char* file)
 {
-	
+	std::fstream fs;
+	int* degrees;
+	fs.open(file, std::fstream::out);
+	fs << this->CountEdges() << std::endl;
+	this->GetDegrees(degrees);
+	for(int i=0; i < this->n; i++)
+	{
+		fs << degrees[i] << " ";
+	}
+	fs << std::endl;
+	fs << this->GetTypeOfGraph();
+	fs.close();
+	delete []degrees;
+	degrees = NULL;
 }
